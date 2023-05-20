fc07459d6fe9bef13d52be649187933fcab9da93
AnaGuelfi <aguelfie@gmail.com>
2023-05-20 10:31:55 -0300

    Criação do Projeto

---
+++ app/src/main/java/br/edu/ifsp/dmo/view/MainActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.dmo.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.dmo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
