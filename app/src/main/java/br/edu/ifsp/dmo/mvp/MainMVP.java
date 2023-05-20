1df358ddf337cc90e54705e0c40ba8378e00e045
AnaGuelfi <aguelfie@gmail.com>
2023-05-20 11:22:00 -0300

    Implementação da MainActivity

---
+++ app/src/main/java/br/edu/ifsp/dmo/mvp/MainMVP.java
@@ -0,0 +1,25 @@
+package br.edu.ifsp.dmo.mvp;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import br.edu.ifsp.dmo.model.entities.Tarefa;
+
+public interface MainMVP {
+    interface View{
+        Context getContext();
+    }
+
+    interface Presenter{
+        void deatach();
+
+        void openDetails();
+
+        void openDetails(Tarefa tarefa);
+
+        void populateList(RecyclerView recyclerView);
+
+        void favoriteTask(Tarefa tarefa);
+    }
+}
