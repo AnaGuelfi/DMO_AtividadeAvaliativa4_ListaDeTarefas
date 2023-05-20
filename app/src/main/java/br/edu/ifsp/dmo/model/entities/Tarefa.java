e0eae7b39e09d3a645228e36b0ce560959a463db
AnaGuelfi <aguelfie@gmail.com>
2023-05-20 11:00:13 -0300

    Classe Tarefa e Versão Inicial da MainActivity

---
+++ app/src/main/java/br/edu/ifsp/dmo/model/entities/Tarefa.java
@@ -0,0 +1,33 @@
+package br.edu.ifsp.dmo.model.entities;
+
+import java.time.LocalDate;
+
+public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataCriacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
